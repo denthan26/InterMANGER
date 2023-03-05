1b9bf44a15f58d2492250b701d3804b879050dff
denchan <2834796821@qq.com>
2023-03-05 23:18:14 +0800

    Java物业管理系统01

---
+++ estate-manager/src/main/java/com/heima/estatemanager/EstateManagerApplication.java
@@ -0,0 +1,13 @@
+package com.heima.estatemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstateManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EstateManagerApplication.class, args);
+    }
+
+}
---
+++ estate-manager/src/test/java/com/heima/estatemanager/EstateManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heima.estatemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstateManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
